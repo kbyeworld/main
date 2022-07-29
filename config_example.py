38048da49258cc3eb35b43c6ecfe7784d4b731c5
SnakeBlanket <bainble0211@naver.com>
2022-07-30 01:40:25 +0900

    style(config_example): Set ClassName with CamelCase

--- config_example.py
+++ config_example.py
@@ -1,7 +1,10 @@
-class bot:
-    token = "" # 봇 토큰을 입력합니다.
-    prefix = [] # 일부 명령어 사용을 위한 접두사를 입력합니다.
+class Bot:
+    token = ""  # 봇 토큰을 입력합니다.
+    prefix = []  # 일부 명령어 사용을 위한 접두사를 입력합니다.
 
-class setting:
-    owner_ids = [] # 봇 관리자의 아이디를 입력합니다.
-    intents = ["members"] # presences (PRESENCE INTENT), members (SERVER MEMBERS INTENT), message_content (MESSAGE CONTENT INTENT)를 입력할 수 있습니다.
+
+class Setting:
+    owner_ids = []  # 봇 관리자의 아이디를 입력합니다.
+    intents = ["members"]
+        # presences (PRESENCE INTENT), members (SERVER MEMBERS INTENT)
+        # message_content (MESSAGE CONTENT INTENT)를 입력할 수 있습니다.
