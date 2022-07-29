cacb0b2516224d010aa91e44e25755c7c5f55b5d
SnakeBlanket <bainble0211@naver.com>
2022-07-30 01:42:04 +0900

    style(bot): Set ClassName with CamelCase

--- bot.py
+++ bot.py
@@ -8,19 +8,19 @@ from discord.ext import commands
 import config
 
 
-class kbyeworld(commands.AutoShardedBot):
+class kByeWorld(commands.AutoShardedBot):
     def __init__(self):
         intents = discord.Intents.default()
-        intents.members = (True if "members" in config.setting.intents else False)
-        intents.presences = (True if "presences" in config.setting.intents else False)
-        intents.message_content = (True if "message_content" in config.setting.intents else False)
+        intents.members = (True if "members" in config.Setting.intents else False)
+        intents.presences = (True if "presences" in config.Setting.intents else False)
+        intents.message_content = (True if "message_content" in config.Setting.intents else False)
         super().__init__(
             command_prefix=(
-                config.bot.prefix
+                config.Bot.prefix
             ),
-            owner_ids=config.setting.owner_ids,
-            help_command=None,
-            intents=intents,
+            owner_ids=config.Setting.owner_ids,
+            intents=intents
+
         )
 
         logger = logging.getLogger("kbyeworld")
@@ -40,17 +40,18 @@ class kbyeworld(commands.AutoShardedBot):
 
         logger.info("📋 | Loading Modules...")
         os.environ["JISHAKU_NO_UNDERSCORE"] = "True"
+
         self.load_extension("jishaku")
-        logger.info("✅ | Success to Load : Jishaku")
-        cogs = [i.replace(".py", "") for i in os.listdir("./cogs") if i.endswith(".py")]
-        cogs += [f"marble.{i.replace('.py', '')}" for i in os.listdir("./cogs/marble") if i.endswith(".py")]
-        for i in cogs:
+
+        files = [f"cogs.{item[:-3]}" for item in os.listdir(f"./cogs/") if os.path.isfile(f"./cogs/{item}") if item.endswith(".py")] \
+                + [f"cogs.{i}.{item[:-3]}" for i in os.listdir("./cogs/") if os.path.isdir(f"./cogs/{i}") for item in os.listdir(f"./cogs/{i}") if item.endswith(".py")]
+        for file in files:
             try:
-                self.load_extension(f"cogs.{i}")
-                logger.info(f"✅ | Success to Load : cogs.{i}")
+                self.load_extension(file)
+                logger.info(f"✅ | Success to Load : {file}")
             except Exception as error:
-                logger.error(f"❎ | Failed to Load : cogs.{i} (reason : {error})")
+                logger.error(f"❎ | Failed to Load : {file} (reason : {error})")
 
 
 if __name__ == "__main__":
-    kbyeworld().run(config.bot.token)
+    kByeWorld().run(config.Bot.token)
