1995788e24ce0641aa7c98fead7e8818670b67dd
SnakeBlanket <bainble0211@naver.com>
2022-07-31 00:50:32 +0900

    feat(cogs.marble.dice): fixing some bugs in dice handle

--- cogs/marble/dice.py
+++ cogs/marble/dice.py
@@ -3,16 +3,17 @@ from discord.ext import commands
 import random
 from utils.respond import send_response
 
+
 class DiceCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_interaction(self, interaction: discord.Interaction):
-        if interaction.custom_id.startswith("dice_"):
-            await interaction.defer(ephemeral=True)
+        if interaction.is_component() and interaction.custom_id.startswith("dice_"):
+            await send_response(interaction,
+                f"<@{interaction.user.id}>님의 주사위는 {str(random.randint(1, 6))}입니다!", delete_after=5)
 
-            await send_response(interaction, f"<@{interaction.custom_id.replace('dice_', '')}>님의 주사위느 {str(random.randint(1, 6))}입니다!")
 
 def setup(bot):
     bot.add_cog(DiceCog(bot))
