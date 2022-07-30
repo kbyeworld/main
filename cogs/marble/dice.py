ab0e5c3a52736e6b7c8773531c9e328e4e421525
Yunseo Jeong <samsunghappytree123@naver.com>
2022-07-31 00:01:26 +0900

    fix(cogs.marble.dice): fix typo

--- cogs/marble/dice.py
+++ cogs/marble/dice.py
@@ -12,7 +12,7 @@ class DiceCog(commands.Cog):
         if interaction.custom_id.startswith("dice_"):
             await interaction.defer(ephemeral=True)
 
-            await send_response(interaction, f"<@{interaction.custom_id.replace('dice_', '')}>{str(random.randint(1, 6)})
+            await send_response(interaction, f"<@{interaction.custom_id.replace('dice_', '')}>님의 주사위느 {str(random.randint(1, 6)}입니다!")
 
 def setup(bot):
     bot.add_cog(DiceCog(bot)
