4da2ac8fcc9396405789e50f2cfdbe802d1b30da
SnakeBlanket <bainble0211@naver.com>
2022-07-30 13:14:46 +0900

    feat(cogs.marble.play): apply josn module and edit some embed

--- cogs/marble/play.py
+++ cogs/marble/play.py
@@ -6,6 +6,7 @@ import json
 import datetime
 
 from utils.embed import Embed
+from utils.json_util import loadjson, savejson
 
 
 class JoinButton(discord.ui.Button):
@@ -18,43 +19,21 @@ class JoinButton(discord.ui.Button):
         )
 
 
-    # async def callback(self, interaction: discord.Interaction):
-    #     await interaction.response.defer()
-    #     print(interaction.user)
-    #     with open("./data/game.json", encoding='utf-8', mode="r") as f:
-    #         mydict = json.loads(f.read())
-    #     print(mydict)
-    #     mydict[interaction.custom_id]["player"].append(interaction.user.id)
-    #     print(mydict)
-    #     print(interaction.custom_id)
-    #     with open("./data/game.json", encoding='utf-8', mode="w") as f:
-    #         json.dump(mydict, f, ensure_ascii=True)
-    #     await interaction.response.send_message(f"참가 처리됨", ephemeral=True)
-
-
 class marble_play(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.join = []
 
         with open("./data/game.json", encoding='utf-8', mode="r") as f:
             mydict = json.loads(f.read())
-        for d in mydict:
-            for member in d['players']: self.join.append(member)
+        self.join = [member for dic in mydict for member in dic["players"]]
 
-    @commands.slash_command(name="gj")
-    async def get_j(self, ctx: discord.ApplicationContext):
-        await ctx.respond(self.join)
-
-    @commands.slash_command(name="시작", description="마블 게이")
+    @commands.slash_command(name="시작", description="마블 게임을 시작합니다.")
     @commands.max_concurrency(1, commands.BucketType.user)
     async def play_start(self, ctx,
-                       multie: Option(str, "플레이 종류를 선택해주세요.", choices=["이 서버에서 게임"], required=False, name="종류")):
+                         multie: Option(str, "플레이 종류를 선택해주세요.", choices=["이 서버에서 게임"], required=False, name="종류")):
         await ctx.defer()
-        print(self.join)
 
-        with open("./data/game.json", encoding='utf-8', mode="r") as f:
-            mydict = json.loads(f.read())
+        mydict = loadjson("./data/game.json")
         try:
             if mydict[str(ctx.author.id)] or ctx.author.id in self.join:
                 return await ctx.respond("이미 생성되거나 참여한 게임이 있습니다!", ephemeral=True)
@@ -72,19 +51,22 @@ class marble_play(commands.Cog):
                               timestamp=datetime.datetime.now())
         Embed.user_footer(embed, ctx.author)
 
-        mydict[ctx.author.id] = {"channel_id": thread.id, "players": [ctx.author.id], "start_at": datetime.datetime.now().timestamp()}
+        mydict[ctx.author.id] = {"channel_id": thread.id, "players": [ctx.author.id],
+                                 "start_at": datetime.datetime.now().timestamp()}
 
-        with open("./data/game.json", encoding='utf-8', mode="w") as f:
-            json.dump(mydict, f, indent=4, ensure_ascii=False)
+        savejson("./data/game.json", mydict)
 
         await thread.add_user(ctx.guild.get_member(ctx.author.id))
         self.join.append(ctx.author.id)
 
         view = discord.ui.View()
         view.add_item(JoinButton(ctx.author))
+        # view.add_item(discord.ui.Button(emoji="✅", label="참가하기", custom_id=f"marble_{ctx.author.id}_join",
+        #                                 style=discord.ButtonStyle.green))
 
         try:
-            await start_msg.edit(content=f"✅ {'이 서버에서' if kind == 'Server' else '글로벌 멀티'} 게임이 시작 준비중이에요.", embed=embed, view=view)
+            await start_msg.edit(content=f"✅ {'이 서버에서' if kind == 'Server' else '글로벌 멀티'} 게임이 시작 준비중이에요.", embed=embed,
+                                 view=view)
         except Exception as error:
             pass
 
@@ -108,17 +90,15 @@ class marble_play(commands.Cog):
                     except discord.Forbidden:
                         await interaction.guild.get_thread(int(game_data["channel_id"])).archive()
                     msg = await interaction.channel.fetch_message(int(game_data["channel_id"]))
-                    await msg.edit(content="⏹ 게임이 취소되었어요!", view=None, embed=Embed.user_footer(Embed.default(timestamp=datetime.datetime.now(), title="⏹ 게임 취소", description="호스트가 게임을 취소하였습니다."), interaction.user))
+                    await msg.edit(content="⏹ 게임이 취소되었어요!", view=None, embed=Embed.user_footer(
+                        Embed.default(timestamp=datetime.datetime.now(), title="⏹ 게임 취소",
+                                      description="호스트가 게임을 취소하였습니다."), interaction.user))
                     del mydict[user_id]
                     json.dump(mydict, open("./data/game.json", encoding='utf-8', mode="w"), ensure_ascii=True)
                     return await interaction.response.send_message(f"게임이 취소되었습니다.", ephemeral=True)
                 if interaction.user.id in game_data["players"]:
                     return await interaction.response.send_message(f"이미 참가처리 되었습니다!", ephemeral=True)
-                print(mydict)
                 game_data["players"].append(interaction.user.id)
-                print(mydict)
-                print(interaction.custom_id)
-                print(interaction.user.id)
                 with open("./data/game.json", encoding='utf-8', mode="w") as f:
                     json.dump(mydict, f, ensure_ascii=True)
                 thread = interaction.guild.get_thread(game_data['channel_id'])
