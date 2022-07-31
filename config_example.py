e322e37e7d30aea66da7ed04e0421e094b601473
SnakeBlanket <bainble0211@naver.com>
2022-07-31 23:11:46 +0900

    feat(config_example): add temporary field. KxD_API_Key

--- config_example.py
+++ config_example.py
@@ -9,6 +9,10 @@ class Setting:
     # presences (PRESENCE INTENT), members (SERVER MEMBERS INTENT)
     # message_content (MESSAGE CONTENT INTENT)를 입력할 수 있습니다.
 
+    KxD_API_Key = ""
+    # KxD 해커톤에서 제공한 API 키를 입력합니다.
+    # 위 필드는 해커톤이 종료된 이후, 삭제될 예정입니다.
+
     class database:
         uri = "mongodb://localhost:27017" # MongoDB 데이터베이스에 접속할 수 있는 URI를 입력합니다.
         name = "kbyeworld" # MongoDB 데이터베이스 이름을 입력합니다.
